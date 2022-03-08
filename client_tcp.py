2806eea6d13b7ee6d50508b20938eb2fa5d4358e
Marcos Yacob <marcos.yacob@hpe.com>
2022-03-08 20:13:53 -0300

    Add tcp client
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- client_tcp.py
+++ client_tcp.py
@@ -3,6 +3,7 @@ from unicodedata import name
 import grpc
 import logging
 from spire.api.server.trustdomain.v1 import trustdomain_pb2_grpc
+from cryptography.hazmat.primitives import serialization
 from spire.api.server.trustdomain.v1 import trustdomain_pb2
 from spire.api.types import federationrelationship_pb2
 from pyspiffe.workloadapi.default_workload_api_client import DefaultWorkloadApiClient
@@ -12,8 +13,18 @@ _LOGGER = logging.getLogger(__name__)
 def main():
     client = DefaultWorkloadApiClient('unix:///tmp/spire-agent/public/api.sock')
     svid = client.fetch_x509_svid()
-    print(svid)
-    with grpc.secure_channel('127.0.0.1:8081', svid.leaf) as channel:
+    leaf = svid.leaf().public_bytes(encoding=serialization.Encoding.PEM)
+    print(leaf)
+    privateKey = svid.private_key().private_bytes(serialization.Encoding.PEM, serialization.PrivateFormat.PKCS8, serialization.NoEncryption())
+    print(privateKey)
+
+    print("\n\n")
+
+
+
+    creds = grpc.ssl_channel_credentials(certificate_chain=leaf, private_key=privateKey)
+
+    with grpc.secure_channel('127.0.0.1:8081', creds) as channel:
     # with grpc.insecure_channel('unix:///tmp/spire-server/private/api.sock') as channel:
         stub = trustdomain_pb2_grpc.TrustDomainStub(channel)
 
