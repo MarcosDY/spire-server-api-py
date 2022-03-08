22d3aba2337d9248689bc14099d156ea44265a4d
Marcos Yacob <marcos.yacob@hpe.com>
2022-03-08 18:34:05 -0300

    missing files
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- client_uds.py
+++ client_uds.py
@@ -9,6 +9,17 @@ _LOGGER = logging.getLogger(__name__)
 def main():
     with grpc.insecure_channel('unix:///tmp/spire-server/private/api.sock') as channel:
         stub = trustdomain_pb2_grpc.TrustDomainStub(channel)
+        print('Create federation relationship')
+        req = trustdomain_pb2.BatchCreateFederationRelationshipRequest()
+        fr = req.federation_relationships.add()
+        fr.trust_domain = "domain.test1"
+        fr.bundle_endpoint_url = "https://upstream-spire-server"
+        fr.https_spiffe.endpoint_spiffe_id = "spiffe://domain.test3/spire/server"
+
+        createResp = stub.BatchCreateFederationRelationship(req)
+        print(createResp)
+
+        print('List federation')
         resp = stub.ListFederationRelationships(trustdomain_pb2.ListFederationRelationshipsRequest())
         print(resp)
 
