37c2e31590ba871d8d80a9e2e3d8779706b3ebcd
Marcos Yacob <marcos.yacob@hpe.com>
2022-03-08 17:57:47 -0300

    add socket client
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

---
+++ client_uds.py
@@ -0,0 +1,17 @@
+from http import client
+import grpc
+import logging
+# import proto.spire.api.server.trustdomain.v1.trustdomain_pb2
+from spire.api.server.trustdomain.v1 import trustdomain_pb2_grpc
+from spire.api.server.trustdomain.v1 import trustdomain_pb2
+
+_LOGGER = logging.getLogger(__name__)
+
+def main():
+    with grpc.insecure_channel('unix:///tmp/spire-server/private/api.sock') as channel:
+        stub = trustdomain_pb2_grpc.TrustDomainStub(channel)
+        resp = stub.ListFederationRelationships(trustdomain_pb2.ListFederationRelationshipsRequest())
+        print(resp)
+
+if __name__ == '__main__':
+    main()
